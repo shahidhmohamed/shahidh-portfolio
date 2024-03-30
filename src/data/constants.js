b646ba43948b56bd5d045a8ba73ad19a50f82b5a
shahidhmohamed <mohamedshahidh81@gmail.com>
2024-03-30 11:49:52 +0530

    Project Add

--- src/data/constants.js
+++ src/data/constants.js
@@ -360,5 +360,17 @@ export const projects = [
     category: "web app",
     github: "https://github.com/shahidhmohamed/todolist",
   },
+  {
+    id: 6,
+    title: "Translator next-app",
+    date: "Feb 2024",
+    description:
+      "Language Translator",
+    image:
+      "https://camo.githubusercontent.com/84ac6ab6f378348ef28d8184062b7e9e3511a1252ae3966eaa49e8e998f732a7/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f74253230283938292e706e673f616c743d6d6564696126746f6b656e3d33643335646366322d626666322d343730382d393031632d343232383866383332386633",
+    tags: ["Next Js", "vercel", "Python", "Node JS"],
+    category: "web app",
+    github: "https://github.com/shahidhmohamed/translator-next-app",
+  },
 ];
 
